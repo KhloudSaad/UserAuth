2c872554c3c4f67cab878691ae7583a529cc0d6c
KhloudSaad <105751738+KhloudSaad@users.noreply.github.com>
2024-11-30 21:18:02 +0200

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using userauth.Models;
+
+namespace userauth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace userauth.Models
+{
+    public class ApplicationUser :IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+
+        [StringLength(250)]
+        public string LastName { get; set; }
+        public string? Id { get; internal set; }
+    }
+}
